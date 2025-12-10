c20045ace2a2ce342fca6292814f7c77740b3e43
Bibis Gr <bertha.garrastacho@digitechfp.com>
2025-12-10 12:57:14 +0100

    signals app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Signal } from './componentes/signal/signal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo: '/signals', pathMatch:'full'},
+    {path: 'signals', component: Signal}
+];
